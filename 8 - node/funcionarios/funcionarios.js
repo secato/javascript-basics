1c64182ca19cb683c16b1a9c8ad87271db231e75
Felipe Secato <felipe.secato@gmail.com>
2018-02-24 19:14:25 -0300

    parametro entre modulos e io de arquivo

--- 8 - node/funcionarios/funcionarios.js
+++ 8 - node/funcionarios/funcionarios.js
@@ -4,5 +4,5 @@ const axios = require('axios')
 
 axios.get(url).then(response => {
     const funcionarios = response.data
-    console.log(funcionarios)
+    // console.log(funcionarios)
 })
---
+++ 8 - node/passandoParametrosClient.js
@@ -0,0 +1,5 @@
+const saudacoes = require('./passandoParametros')('Ana', 'Lucas')
+
+saudacoes.forEach(pessoa => {
+    console.log(pessoa)
+});
