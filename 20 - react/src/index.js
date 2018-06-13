8e9705d2978987a6900db4b10c1fcf8a69ecd7f2
Felipe Secato <felipe.secato@ufes.br>
2018-06-13 15:27:27 -0300

    exemplo utilizando classe, estado e bind

--- 20 - react/src/index.js
+++ 20 - react/src/index.js
@@ -7,12 +7,14 @@ import ReactDOM from 'react-dom'
 // const elemento = <h1>React app</h1>
 
 // import {BoaTarde, BoaNoite} from './components/Multiplos'
-import Multi from './components/Multiplos'
+// import Multi from './components/Multiplos'
+import Saudacao from './components/Saudacao'
 
 ReactDOM.render(
     <div>
-        <Multi.BoaTarde nome="Felipe" idade={29} />
-        <Multi.BoaNoite nome='Ana' />
+        {/* <Multi.BoaTarde nome="Felipe" idade={29} />
+        <Multi.BoaNoite nome='Ana' /> */}
+        <Saudacao tipo="Boa noite" nome="Felipe" />
     </div>,
     document.getElementById('root')
 )
